1ef69c3fd7ae10efeae88f9ff002d76974515f76
Minh Than <thanqminh@gmail.com>
2016-07-12 01:59:07 +0700

    add angular2-rails5-unified

---
+++ angular2-in-rails5/app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def index
+    respond_to do |format|
+      format.json { render json: {some: 'data'} }
+    end
+  end
+end
